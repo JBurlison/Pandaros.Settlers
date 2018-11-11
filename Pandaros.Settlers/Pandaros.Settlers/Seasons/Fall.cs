90c81b498c9c3c557ef0ba0798aeece31a4cd766
Jim Burlison <JBurlison@Gmail.com>
2018-11-11 10:06:13 -0500

    Cleaned up usings

--- Pandaros.Settlers/Pandaros.Settlers/Seasons/Fall.cs
+++ Pandaros.Settlers/Pandaros.Settlers/Seasons/Fall.cs
@@ -1,5 +1,4 @@
-﻿using BlockTypes;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Pandaros.Settlers.Seasons
 {
--- Pandaros.Settlers/Pandaros.Settlers/Seasons/SeasonsFactory.cs
+++ Pandaros.Settlers/Pandaros.Settlers/Seasons/SeasonsFactory.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TerrainGeneration;
-using Math = System.Math;
 
 namespace Pandaros.Settlers.Seasons
 {
