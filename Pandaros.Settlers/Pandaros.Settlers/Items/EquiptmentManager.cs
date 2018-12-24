026e8167fc7b5b37e0c63695fa8f776e703fbe72
Jim Burlison <JBurlison@Gmail.com>
2018-12-24 13:42:23 -0500

    more equitpment manager

--- Pandaros.Settlers/Pandaros.Settlers/Items/EquiptmentManager.cs
+++ Pandaros.Settlers/Pandaros.Settlers/Items/EquiptmentManager.cs
@@ -1,4 +1,5 @@
-﻿using NetworkUI;
+﻿using BlockTypes;
+using NetworkUI;
 using NetworkUI.Items;
 using Pandaros.Settlers.ColonyManager;
 using System;
@@ -53,7 +54,7 @@ namespace Pandaros.Settlers.Items
                         }
 
                         NetworkMenuManager.SendServerPopup(data.Player, menu);
-                        break;
+                        return;
                     }
             }
             else if (data.ButtonIdentifier.Contains(".EquiptmentButton"))
@@ -66,7 +67,130 @@ namespace Pandaros.Settlers.Items
                         foreach (var job in jobKvp.Value.TakenJobs)
                             if (data.ButtonIdentifier.Contains("." + job.NPC.ID.ToString() + ".EquiptmentButton"))
                             {
+                                BuildSettlerDetailMenu(data, jobKvp, job);
+                                return;
+                            }
+                    }
+            }
+            else if (data.ButtonIdentifier.Contains(".AddEquiptmentButton"))
+            {
+                NetworkMenu menu = new NetworkMenu();
+                menu.LocalStorage.SetAs("header", "Equiptment");
+                menu.Width = 1000;
+                menu.Height = 600;
+                var newButtonID = data.ButtonIdentifier.Replace(".AddEquiptmentButton", "");
 
+                if (data.ButtonIdentifier.Contains(".wep."))
+                {
+                    foreach (var kvp in data.Player.ActiveColony.Stockpile.Items)
+                    {
+                        if (kvp.Value > 0 && Weapons.WeaponFactory.WeaponLookup.TryGetValue(kvp.Key, out var wepItem))
+                        {
+                            List<IItem> items = new List<IItem>();
+                            items.Add(new ItemIcon(kvp.Key));
+                            items.Add(new Label(new LabelData(wepItem.Name, UnityEngine.Color.black)));
+                            items.Add(new ButtonCallback(newButtonID + "." + kvp.Key + ".AddSelectedEquiptmentButton", new LabelData("Select", UnityEngine.Color.black)));
+                            menu.Items.Add(new HorizontalGrid(items, 330));
+                        }
+                    }
+                }
+                else if (data.ButtonIdentifier.Contains(".arm."))
+                {
+                    foreach (var kvp in data.Player.ActiveColony.Stockpile.Items)
+                    {
+                        if (kvp.Value > 0 && Armor.ArmorFactory.ArmorLookup.TryGetValue(kvp.Key, out var armItem))
+                        {
+                            List<IItem> items = new List<IItem>();
+                            items.Add(new ItemIcon(kvp.Key));
+                            items.Add(new Label(new LabelData(armItem.Name, UnityEngine.Color.black)));
+                            items.Add(new ButtonCallback(newButtonID + "." + kvp.Key + ".AddSelectedEquiptmentButton", new LabelData("Select", UnityEngine.Color.black)));
+                            menu.Items.Add(new HorizontalGrid(items, 330));
+                        }
+                    }
+                }
+
+                NetworkMenuManager.SendServerPopup(data.Player, menu);
+                return;
+            }
+            else if (data.ButtonIdentifier.Contains(".AddSelectedEquiptmentButton"))
+            {
+                Dictionary<string, JobCounts> jobCounts = ColonyTool.GetJobCounts(data.Player.ActiveColony);
+
+                foreach (var jobKvp in jobCounts)
+                    if (data.ButtonIdentifier.Contains(jobKvp.Key))
+                    {
+                        foreach (var job in jobKvp.Value.TakenJobs)
+                            if (data.ButtonIdentifier.Contains("." + job.NPC.ID.ToString() + "."))
+                            {
+                                if (data.ButtonIdentifier.Contains(".wep."))
+                                {
+                                    foreach (var kvp in data.Player.ActiveColony.Stockpile.Items)
+                                    {
+                                        if (Weapons.WeaponFactory.WeaponLookup.TryGetValue(kvp.Key, out var wepItem) && data.ButtonIdentifier.Contains("." + kvp.Key + "."))
+                                        {
+                                            var inv = Entities.SettlerInventory.GetSettlerInventory(job.NPC);
+                                            inv.Weapon.Id = kvp.Key;
+                                            inv.Weapon.Durability = wepItem.Durability;
+                                            BuildSettlerDetailMenu(data, jobKvp, job);
+                                            return;
+                                        }
+                                    }
+                                }
+                                else if (data.ButtonIdentifier.Contains(".arm."))
+                                {
+                                    foreach (var kvp in data.Player.ActiveColony.Stockpile.Items)
+                                    {
+                                        if (Armor.ArmorFactory.ArmorLookup.TryGetValue(kvp.Key, out var armItem) && data.ButtonIdentifier.Contains("." + kvp.Key + ".") && data.Player.ActiveColony.Stockpile.TryRemove(kvp.Key))
+                                        {
+                                            var inv = Entities.SettlerInventory.GetSettlerInventory(job.NPC);
+                                            inv.Armor[armItem.Slot].Id = kvp.Key;
+                                            inv.Armor[armItem.Slot].Durability = armItem.Durability;
+                                            BuildSettlerDetailMenu(data, jobKvp, job);
+                                            return;
+                                        }
+                                    }
+                                }
+                            }
+                    }
+            }
+            else if (data.ButtonIdentifier.Contains(".RemoveEquiptmentButton"))
+            {
+                Dictionary<string, JobCounts> jobCounts = ColonyTool.GetJobCounts(data.Player.ActiveColony);
+
+                foreach (var jobKvp in jobCounts)
+                    if (data.ButtonIdentifier.Contains(jobKvp.Key))
+                    {
+                        foreach (var job in jobKvp.Value.TakenJobs)
+                            if (data.ButtonIdentifier.Contains("." + job.NPC.ID.ToString() + ".EquiptmentButton"))
+                            {
+                                var inv = Entities.SettlerInventory.GetSettlerInventory(job.NPC);
+
+                                if (data.ButtonIdentifier.Contains(".wep."))
+                                {
+                                    if (inv.Weapon.Id != default(ushort))
+                                        job.NPC.Colony.Stockpile.Add(inv.Weapon.Id);
+
+                                    inv.Weapon.Id = default(ushort);
+                                    inv.Weapon.Durability = default(int);
+                                }
+                                else if (data.ButtonIdentifier.Contains(".arm."))
+                                {
+                                    foreach (var armor in inv.Armor)
+                                    {
+                                        if (data.ButtonIdentifier.Contains("." + armor.Key + "."))
+                                        {
+                                            if (armor.Value.Id != default(ushort))
+                                                job.NPC.Colony.Stockpile.Add(armor.Value.Id);
+
+                                            armor.Value.Id = default(ushort);
+                                            armor.Value.Durability = default(int);
+                                            break;
+                                        }
+                                    }
+                                }
+
+                                BuildSettlerDetailMenu(data, jobKvp, job);
+                                return;
                             }
                     }
             }
@@ -75,6 +199,60 @@ namespace Pandaros.Settlers.Items
                 Dictionary<string, JobCounts> jobCounts = ColonyTool.GetJobCounts(data.Player.ActiveColony);
                 NetworkMenuManager.SendServerPopup(data.Player, ColonyTool.BuildMenu(data.Player, jobCounts, false, string.Empty, 0));
             }
+        }
+
+        public static void BuildSettlerDetailMenu(ButtonPressCallbackData data, KeyValuePair<string, JobCounts> jobKvp, global::Jobs.IJob job)
+        {
+            var inv = Entities.SettlerInventory.GetSettlerInventory(job.NPC);
+            NetworkMenu menu = new NetworkMenu();
+            menu.LocalStorage.SetAs("header", inv.SettlerName);
+            menu.Width = 1000;
+            menu.Height = 600;
+
+            List<IItem> wep = new List<IItem>();
+            wep.Add(new Label(new LabelData("Weapon", UnityEngine.Color.black)));
+            wep.Add(new ItemIcon(inv.Weapon.Id));
+
+            if (Weapons.WeaponFactory.WeaponLookup.TryGetValue(inv.Weapon.Id, out var wepItem))
+            {
+                wep.Add(new Label(new LabelData(wepItem.Name, UnityEngine.Color.black, UnityEngine.TextAnchor.MiddleLeft, 18, LabelData.ELocalizationType.Type)));
+                wep.Add(new ButtonCallback(jobKvp.Key + ".wep." + job.NPC.ID + ".AddEquiptmentButton", new LabelData("Swap", UnityEngine.Color.black)));
+                wep.Add(new ButtonCallback(jobKvp.Key + ".wep." + job.NPC.ID + ".RemoveEquiptmentButton", new LabelData("Remove", UnityEngine.Color.black)));
+            }
+            else
+            {
+                wep.Add(new Label(new LabelData("", UnityEngine.Color.black)));
+
+                if (inv.Weapon.Id != default(ushort))
+                    wep.Add(new ButtonCallback(jobKvp.Key + ".wep." + job.NPC.ID + ".AddEquiptmentButton", new LabelData("Swap", UnityEngine.Color.black)));
+            }
+
+            menu.Items.Add(new HorizontalGrid(wep, 200));
+
+            foreach (var armor in inv.Armor)
+            {
+                List<IItem> items = new List<IItem>();
+                items.Add(new Label(new LabelData(armor.Key.ToString(), UnityEngine.Color.black)));
+                items.Add(new ItemIcon(armor.Value.Id));
+
+                if (Armor.ArmorFactory.ArmorLookup.TryGetValue(armor.Value.Id, out var arm))
+                {
+                    items.Add(new Label(new LabelData(arm.Name, UnityEngine.Color.black, UnityEngine.TextAnchor.MiddleLeft, 18, LabelData.ELocalizationType.Type)));
+                    items.Add(new ButtonCallback(jobKvp.Key + "." + armor.Key + ".arm." + job.NPC.ID + ".AddEquiptmentButton", new LabelData("Swap", UnityEngine.Color.black)));
+                    items.Add(new ButtonCallback(jobKvp.Key + "." + armor.Key + ".arm." + job.NPC.ID + ".RemoveEquiptmentButton", new LabelData("Remove", UnityEngine.Color.black)));
+                }
+                else
+                {
+                    items.Add(new Label(new LabelData("", UnityEngine.Color.black)));
+
+                    if (armor.Value.Id != default(ushort))
+                        items.Add(new ButtonCallback(jobKvp.Key + "." + armor.Key + ".arm." + job.NPC.ID + ".AddEquiptmentButton", new LabelData("Swap", UnityEngine.Color.black)));
+                }
+
+                menu.Items.Add(new HorizontalGrid(items, 200));
+            }
+
+            NetworkMenuManager.SendServerPopup(data.Player, menu);
         }
     }
 }
