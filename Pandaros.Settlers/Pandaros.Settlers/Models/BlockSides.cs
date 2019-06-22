6939714211d49fae6ac9f91d78a4abed76ae47c7
Jim Burlison <JBurlison@Gmail.com>
2019-06-21 22:01:49 -0400

    Fixed block state.

--- Pandaros.Settlers/Pandaros.Settlers/Models/BlockSides.cs
+++ Pandaros.Settlers/Pandaros.Settlers/Models/BlockSides.cs
@@ -8,41 +8,31 @@ namespace Pandaros.Settlers.Models
 {
     public enum BlockSides
     {
-        XpZpYp = BitWiseBlockSides.Xp & BitWiseBlockSides.Zp & BitWiseBlockSides.Yp,
-        XpYp = BitWiseBlockSides.Xp & BitWiseBlockSides.Yp,
-        XpZnYp = BitWiseBlockSides.Xp & BitWiseBlockSides.Zn & BitWiseBlockSides.Yp,
-        ZpYp = BitWiseBlockSides.Zp & BitWiseBlockSides.Yp,
-        Yp = BitWiseBlockSides.Yp,
-        ZnYp = BitWiseBlockSides.Zn & BitWiseBlockSides.Yp,
-        XnZpYp = BitWiseBlockSides.Xn & BitWiseBlockSides.Zp & BitWiseBlockSides.Yp,
-        XnYp = BitWiseBlockSides.Xn & BitWiseBlockSides.Yp,
-        XnZnYp = BitWiseBlockSides.Xn & BitWiseBlockSides.Zn & BitWiseBlockSides.Yp,
-        XpZp = BitWiseBlockSides.Xp & BitWiseBlockSides.Zp,
-        Xp = BitWiseBlockSides.Xp,
-        XpZn = BitWiseBlockSides.Xp & BitWiseBlockSides.Zn,
-        Zp = BitWiseBlockSides.Zp,
-        Zn = BitWiseBlockSides.Zn,
-        XnZp = BitWiseBlockSides.Xn & BitWiseBlockSides.Zp,
-        Xn = BitWiseBlockSides.Xn,
-        XnZn = BitWiseBlockSides.Xn & BitWiseBlockSides.Zn,
-        XpZpYn = BitWiseBlockSides.Xp & BitWiseBlockSides.Zp & BitWiseBlockSides.Yn,
-        XpYn = BitWiseBlockSides.Xp & BitWiseBlockSides.Yn,
-        XpZnYn = BitWiseBlockSides.Xp & BitWiseBlockSides.Zn & BitWiseBlockSides.Yn,
-        ZpYn = BitWiseBlockSides.Zp & BitWiseBlockSides.Yn,
-        Yn = BitWiseBlockSides.Yn,
-        ZnYn = BitWiseBlockSides.Zn & BitWiseBlockSides.Yn,
-        XnZpYn = BitWiseBlockSides.Yn & BitWiseBlockSides.Zp & BitWiseBlockSides.Yn,
-        XnYn = BitWiseBlockSides.Xn & BitWiseBlockSides.Yn,
-        XnZnYn = BitWiseBlockSides.Yn & BitWiseBlockSides.Zn & BitWiseBlockSides.Yn
-    }
-
-    public enum BitWiseBlockSides
-    {
-        Xp = 0,
-        Xn = 1,
-        Zp = 2 << 0,
-        Zn = 2 << 1,
-        Yp = 2 << 2,
-        Yn = 2 << 3
+        XpZpYp,
+        XpYp,
+        XpZnYp,
+        ZpYp,
+        Yp,
+        ZnYp,
+        XnZpYp,
+        XnYp,
+        XnZnYp,
+        XpZp,
+        Xp,
+        XpZn,
+        Zp,
+        Zn,
+        XnZp,
+        Xn,
+        XnZn,
+        XpZpYn,
+        XpYn,
+        XpZnYn,
+        ZpYn,
+        Yn,
+        ZnYn,
+        XnZpYn,
+        XnYn,
+        XnZnYn
     }
 }
