8e3237b775685629f5361d83d93126614ddd9320
Jim Burlison <JBurlison@Gmail.com>
2019-06-28 08:41:06 -0400

    Added required Attributes

--- Pandaros.Settlers/Pandaros.Settlers/Models/BlockSides.cs
+++ Pandaros.Settlers/Pandaros.Settlers/Models/BlockSides.cs
@@ -11,33 +11,33 @@ namespace Pandaros.Settlers.Models
     {
         [BlockSideVectorValues(0, 0, 0)]
         Invalid,
-        [BlockSideVectorValues(1, 1, 0)]
+        [BlockSideVectorValues(1, 1, 0, Xp)]
         XpYp,
-        [BlockSideVectorValues(0, 1, 1)]
+        [BlockSideVectorValues(0, 1, 1, Zp)]
         ZpYp,
         [BlockSideVectorValues(0, 1, 0)]
         Yp,
-        [BlockSideVectorValues(0, 1, -1)]
+        [BlockSideVectorValues(0, 1, -1, Zn)]
         ZnYp,
-        [BlockSideVectorValues(-1, 1, 0)]
+        [BlockSideVectorValues(-1, 1, 0, Xn)]
         XnYp,
-        [BlockSideVectorValues(1, 0, 0)]
+        [BlockSideVectorValues(1, 0, 0, XpYp, XpYn)]
         Xp,
-        [BlockSideVectorValues(0, 0, 1)]
+        [BlockSideVectorValues(0, 0, 1, ZpYp, ZpYn)]
         Zp,
-        [BlockSideVectorValues(0, 0, -1)]
+        [BlockSideVectorValues(0, 0, -1, ZnYp, ZnYn)]
         Zn,
-        [BlockSideVectorValues(-1, 0, 0)]
+        [BlockSideVectorValues(-1, 0, 0, XnYp, XnYn)]
         Xn,
-        [BlockSideVectorValues(1, -1, 0)]
+        [BlockSideVectorValues(1, -1, 0, Xp)]
         XpYn,
-        [BlockSideVectorValues(0, -1, 1)]
+        [BlockSideVectorValues(0, -1, 1, Zp)]
         ZpYn,
         [BlockSideVectorValues(0, -1, 0)]
         Yn,
-        [BlockSideVectorValues(0, -1, -1)]
+        [BlockSideVectorValues(0, -1, -1, Zn)]
         ZnYn,
-        [BlockSideVectorValues(-1, -1, 0)]
+        [BlockSideVectorValues(-1, -1, 0, Xn)]
         XnYn
     }
 }
