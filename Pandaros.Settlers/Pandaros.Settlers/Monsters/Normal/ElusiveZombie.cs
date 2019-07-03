a1720a3bd16f6182fa652db3c299381d81a57e1a
Jim Burlison <JBurlison@Gmail.com>
2019-07-03 08:23:46 -0400

    Added Elusive, jogger and tough zombies

---
+++ Pandaros.Settlers/Pandaros.Settlers/Monsters/Normal/ElusiveZombie.cs
@@ -0,0 +1,90 @@
+﻿using AI;
+using Monsters;
+using NPC;
+using Pandaros.Settlers.Entities;
+using Pandaros.Settlers.Models;
+using Pandaros.Settlers.Monsters.Normal;
+using Pipliz;
+using Pipliz.JSON;
+using Shared;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pandaros.Settlers.Monsters.Normal
+{
+    [ModLoader.ModManager]
+    public class ElusiveZombie : Zombie, IPandaZombie
+    {
+        public static string Key = GameLoader.NAMESPACE + ".Monsters.Bosses.ElusiveZombie";
+        private static NPCTypeMonsterSettings _mts;
+        private double _cooldown = 2;
+        private float _totalHealth = 1000;
+
+        public ElusiveZombie() :
+            base(NPCType.GetByKeyNameOrDefault(Key), new Path(), GameLoader.StubColony)
+        {
+        }
+
+        public ElusiveZombie(Path path, Colony originalGoal) :
+            base(NPCType.GetByKeyNameOrDefault(Key), path, originalGoal)
+        {
+            TotalHealth = _totalHealth;
+            CurrentHealth = _totalHealth;
+        }
+
+        public IPandaZombie GetNewInstance(Path path, Colony p)
+        {
+            return new ElusiveZombie(path, p);
+        }
+
+
+        public string name => "ElusiveZombie";
+        public override float TotalHealth => _totalHealth;
+        public int MinColonists => 100;
+        public bool KilledBefore
+        {
+            get => false;
+            set => killedBefore = value;
+        }
+
+        public float ZombieHPBonus => 0;
+        public float MissChance => 0.5f;
+        public string MosterType => "PandaZombie";
+
+        public Dictionary<DamageType, float> Damage { get; } = new Dictionary<DamageType, float>
+        {
+            {DamageType.Physical, 50f}
+        };
+
+        public DamageType ElementalArmor => DamageType.Physical;
+
+        public Dictionary<DamageType, float> AdditionalResistance { get; } = new Dictionary<DamageType, float>
+        {
+            {DamageType.Physical, 0.15f}
+        };
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterItemTypesDefined,  GameLoader.NAMESPACE + ".Monsters.Normal.ElusiveZombie.Register")]
+        [ModLoader.ModCallbackDependsOn("pipliz.server.loadnpctypes")]
+        [ModLoader.ModCallbackProvidesFor("pipliz.server.registermonstertextures")]
+        public static void Register()
+        {
+            var m = new JSONNode()
+                   .SetAs("keyName", Key)
+                   .SetAs("printName", "Elusive Zombie")
+                   .SetAs("npcType", "monster");
+
+            var ms = new JSONNode()
+                    .SetAs("albedo", GameLoader.BLOCKS_NPC_PATH + "Phase.png")
+                    .SetAs("normal", GameLoader.BLOCKS_NPC_PATH + "ZombieQueen_normal.png")
+                    .SetAs("emissive", GameLoader.BLOCKS_NPC_PATH + "ZombieQueen_emissive.png")
+                    .SetAs("initialHealth", 1000)
+                    .SetAs("movementSpeed", 1f)
+                    .SetAs("punchCooldownMS", 2000)
+                    .SetAs("punchDamage", 50);
+
+            m.SetAs("data", ms);
+            _mts = new NPCTypeMonsterSettings(m);
+            NPCType.AddSettings(_mts);
+        }
+    }
+}
---
+++ Pandaros.Settlers/Pandaros.Settlers/Monsters/Normal/JoggerZombie.cs
@@ -0,0 +1,90 @@
+﻿using AI;
+using Monsters;
+using NPC;
+using Pandaros.Settlers.Entities;
+using Pandaros.Settlers.Models;
+using Pandaros.Settlers.Monsters.Normal;
+using Pipliz;
+using Pipliz.JSON;
+using Shared;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pandaros.Settlers.Monsters.Normal
+{
+    [ModLoader.ModManager]
+    public class JoggerZombie : Zombie, IPandaZombie
+    {
+        public static string Key = GameLoader.NAMESPACE + ".Monsters.Bosses.JoggerZombie";
+        private static NPCTypeMonsterSettings _mts;
+        private double _cooldown = 2;
+        private float _totalHealth = 500;
+
+        public JoggerZombie() :
+            base(NPCType.GetByKeyNameOrDefault(Key), new Path(), GameLoader.StubColony)
+        {
+        }
+
+        public JoggerZombie(Path path, Colony originalGoal) :
+            base(NPCType.GetByKeyNameOrDefault(Key), path, originalGoal)
+        {
+            TotalHealth = _totalHealth;
+            CurrentHealth = _totalHealth;
+        }
+
+        public IPandaZombie GetNewInstance(Path path, Colony p)
+        {
+            return new JoggerZombie(path, p);
+        }
+
+
+        public string name => "JoggerZombie";
+        public override float TotalHealth => _totalHealth;
+        public int MinColonists => 100;
+        public bool KilledBefore
+        {
+            get => false;
+            set => killedBefore = value;
+        }
+
+        public float ZombieHPBonus => 0;
+        public float MissChance => 0.05f;
+        public string MosterType => "PandaZombie";
+
+        public Dictionary<DamageType, float> Damage { get; } = new Dictionary<DamageType, float>
+        {
+            {DamageType.Physical, 50f}
+        };
+
+        public DamageType ElementalArmor => DamageType.Physical;
+
+        public Dictionary<DamageType, float> AdditionalResistance { get; } = new Dictionary<DamageType, float>
+        {
+            {DamageType.Physical, 0.15f}
+        };
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterItemTypesDefined,  GameLoader.NAMESPACE + ".Monsters.Normal.JoggerZombie.Register")]
+        [ModLoader.ModCallbackDependsOn("pipliz.server.loadnpctypes")]
+        [ModLoader.ModCallbackProvidesFor("pipliz.server.registermonstertextures")]
+        public static void Register()
+        {
+            var m = new JSONNode()
+                   .SetAs("keyName", Key)
+                   .SetAs("printName", "Jogger Zombie")
+                   .SetAs("npcType", "monster");
+
+            var ms = new JSONNode()
+                    .SetAs("albedo", GameLoader.BLOCKS_NPC_PATH + "JackbNimble.png")
+                    .SetAs("normal", GameLoader.BLOCKS_NPC_PATH + "ZombieQueen_normal.png")
+                    .SetAs("emissive", GameLoader.BLOCKS_NPC_PATH + "ZombieQueen_emissive.png")
+                    .SetAs("initialHealth", 500)
+                    .SetAs("movementSpeed", 1.6f)
+                    .SetAs("punchCooldownMS", 2000)
+                    .SetAs("punchDamage", 50);
+
+            m.SetAs("data", ms);
+            _mts = new NPCTypeMonsterSettings(m);
+            NPCType.AddSettings(_mts);
+        }
+    }
+}
