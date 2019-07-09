7187e89f9f4f48e25c7d68a0240253806d630ba5
Jim Burlison <JBurlison@Gmail.com>
2019-07-09 17:54:39 -0400

    Added Overrides to permissions

--- Pandaros.Settlers/Pandaros.Settlers/ColonyManagement/Permission.cs
+++ Pandaros.Settlers/Pandaros.Settlers/ColonyManagement/Permission.cs
@@ -15,9 +15,13 @@ namespace Pandaros.Settlers.ColonyManagement
         [ModLoader.ModCallback(ModLoader.EModCallbackType.OnTryChangeBlock, GameLoader.NAMESPACE + ".ColonyManager.Permission.OnTryChangeBlockUser")]
         public static void OnTryChangeBlockUser(ModLoader.OnTryChangeBlockData userData)
         {
-            if (ServerManager.BlockEntityTracker.BannerTracker.TryGetClosest(userData.Position, out BannerTracker.Banner existingBanner, ServerManager.ServerSettings.Colony.ExclusiveRadius * 2))
+            if (SettlersConfiguration.GetorDefault("AntigriefEnabled", true) && 
+                ServerManager.BlockEntityTracker.BannerTracker.TryGetClosest(userData.Position, out BannerTracker.Banner existingBanner, ServerManager.ServerSettings.Colony.ExclusiveRadius * 2))
             {
-                if (userData.RequestOrigin.Type == BlockChangeRequestOrigin.EType.Player && !existingBanner.Colony.Owners.Contains(userData.RequestOrigin.AsPlayer))
+                if (userData.RequestOrigin.Type == BlockChangeRequestOrigin.EType.Player && 
+                    !PermissionsManager.HasPermission(userData.RequestOrigin.AsPlayer, new PermissionsManager.Permission(GameLoader.NAMESPACE + ".Permissions.Antigrief")) &&
+                    !PermissionsManager.HasPermission(userData.RequestOrigin.AsPlayer, new PermissionsManager.Permission("god")) &&
+                    !existingBanner.Colony.Owners.Contains(userData.RequestOrigin.AsPlayer))
                 {
                     PandaChat.SendThrottle(userData.RequestOrigin.AsPlayer, _LocalizationHelper.LocalizeOrDefault("NotYourColony",userData.RequestOrigin.AsPlayer) + string.Join(", ", existingBanner.Colony.Owners.Select(o => o.Name)) , ChatColor.red);
                     userData.InventoryItemResults.Clear();
