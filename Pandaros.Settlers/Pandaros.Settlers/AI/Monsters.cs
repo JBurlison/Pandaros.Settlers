1020726bbaadcd6be89cc6bff10b607f2c583da1
Jim Burlison <JBurlison@Gmail.com>
2019-07-20 09:35:48 -0400

    Added zombie moan

---
+++ Pandaros.Settlers/Pandaros.Settlers/AI/Monsters.cs
@@ -0,0 +1,34 @@
+﻿using Monsters;
+using Pandaros.API.Monsters;
+
+namespace Pandaros.Settlers.AI
+{
+    [ModLoader.ModManager]
+    public class Monsters
+    {
+        private static double _nextUpdateTime;
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnUpdate, GameLoader.NAMESPACE + ".Managers.MonsterManager.Update")]
+        public static void OnUpdate()
+        {
+            if (!World.Initialized)
+                return;
+
+            var secondsSinceStartDouble = Pipliz.Time.SecondsSinceStartDouble;
+
+            if (_nextUpdateTime < secondsSinceStartDouble)
+            {
+                IMonster m = null;
+
+                foreach (var monster in MonsterManager.GetAllMonsters())
+                    if (m == null || UnityEngine.Vector3.Distance(monster.Value.Position, m.Position) > 15 && Pipliz.Random.NextBool())
+                    {
+                        m = monster.Value;
+                        AudioManager.SendAudio(monster.Value.Position, GameLoader.NAMESPACE + ".ZombieAudio");
+                    }
+
+                _nextUpdateTime = secondsSinceStartDouble + 5;
+            }
+        }
+    }
+}
