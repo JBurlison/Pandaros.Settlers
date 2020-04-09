be8f900a10ef24a29a82ae619bcbca21b4e07791
Jim Burlison <JBurlison@Gmail.com>
2020-04-09 07:52:16 -0400

    Updates!

--- Pandaros.Settlers/Pandaros.Settlers/Properties/AssemblyInfo.cs
+++ Pandaros.Settlers/Pandaros.Settlers/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.54.0405.39113")]
+[assembly: AssemblyVersion("0.57.0409.21218")]
